c61faaece1ca3cd74144488398859d50cbd1bc7c
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2025-06-27 15:51:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -211,11 +211,7 @@ with pan:
     jq = jieqi.jq(y, m, d, h, min)
     pan = kinwuzhao.five_zhao_paipan(qgz[2][0])
     svg_markup = build_svg(pan)
-    
-    # Debug: Display raw SVG markup
-    with st.expander("Debug: Raw SVG Markup"):
-        st.code(svg_markup)
-    
+
     a = "日期︰{}年{}月{}日{}時{}分\n".format(y, m, d, h, min)
     c = "節氣︰{}\n".format(jq)
     d = "干支︰{}年 {}月 {}日 {}時 {}分\n".format(qgz[0], qgz[1], qgz[2], qgz[3], qgz[4])
