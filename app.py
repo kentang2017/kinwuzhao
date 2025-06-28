d0669ab646d17a4ac9fba0d4142c855565c9e6b3
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2025-06-28 23:27:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ with pan:
         pan = kinwuzhao.five_zhao_paipan(qgz[4][0], number)
     svg_markup = build_svg(pan)
 
-    a = "日期︰{}年{}月{}日{}時{}分\n".format(y, m, d, h, min)
+    a = "日期︰{}年{}月{}日{}時{}分   數字:{}\n".format(y, m, d, h, min, number)
     c = "節氣︰{}\n".format(jq)
     d = "干支︰{}年 {}月 {}日 {}時 {}分\n".format(qgz[0], qgz[1], qgz[2], qgz[3], qgz[4])
     
